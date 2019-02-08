8d36fce4f48c1a4c4931884941cac89ec8a05265
DevTiagoCruz <47451155+DevTiagoCruz@users.noreply.github.com>
2019-02-08 14:30:49 +0000

    Update nonioblocker.js

--- nonioblocker.js
+++ nonioblocker.js
@@ -1,10 +1,12 @@
 var triesCookies = 0;
+var triesLogin = 0;
 var triesNonio = 0;
 window.onload = function removeAll() {
 	if(document.URL.indexOf("cmjornal.pt") >= 0){ 
 		remCookiesCM();
-		remNonio();
-	}
+		remLoginCM();
+	};
+	remNonio();
 };
 
 function remNonio() {
@@ -27,6 +29,23 @@ function remNonio() {
 };
 function remCookiesCM() {
 	console.log("REMOVE COOKIES - https://www.cmjornal.pt - tries=" + triesCookies);
+	triesCookies = triesCookies + 1;
+	try {
+		var elem = document.getElementsByClassName("qc-cmp-ui-container qc-cmp-showing");
+		elem[0].remove();
+		document.body.style.overflow = "auto";
+		triesCookies = 0;
+	} catch(erro) {
+		console.log("erro=" + erro);
+		if (triesCookies < 5) {
+			setTimeout(function() {
+				remCookiesCM();
+			}, 500);
+		}
+	}
+};
+function remLoginCM() {
+	console.log("REMOVE LOGIN - https://www.cmjornal.pt - tries=" + triesCookies);
 	triesCookies = triesCookies + 1;
 	try {
 		var elem = document.getElementsByClassName("qc-cmp-ui-container qc-cmp-showing");
